e29051234af4c1664afe30641f71794fb30cace6
Marc Juvénal <marcjuvenal95@gmail.com>
2024-12-14 18:15:24 +0100

    Correction script

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,39 @@
-//DOM Elements
-const viewsContainer = document.querySelector('span.pricing-card_views')
-const rangeInput = document.querySelector('input.pricing-card_views-range')
-const costContainer = document.querySelector('span.pricing-card_price')
-const priceFreqContainer = document.querySelector('span.pricing-card_billing-frequency')
+// DOM Elements
+const viewsContainer = document.querySelector('span.pricing-card__views')
+const rangeInput = document.querySelector('input.pricing-card__views-range')
+const costContainer = document.querySelector('span.pricing-card__price')
+const priceFreqContainer = document.querySelector('span.pricing-card__billing-frequency')
 const frequencyToggle = document.querySelector('input#billing-freq')
-const  form = document.querySelector('form.pricing-card')
+const form = document.querySelector('form.pricing-card')
 
-console.log({rangeInput, viewsContainer, costContainer, priceFreqContainer,
-    frequencyToggle, form})
+console.log({rangeInput, viewsContainer, costContainer, priceFreqContainer, frequencyToggle, form})
 
-//Data & Global Variables
+
+
+// Data & Global Variables
 const VIEWS_DATA = [
     {
-        pageViews:'10K',
+        pageViews: '10k',
         monthlyCost: 8,
         leftPercentage: 0
     },
     {
-        pageViews:'50K',
+        pageViews: '50k',
         monthlyCost: 12,
         leftPercentage: 25
     },
     {
-        pageViews:'100K',
+        pageViews: '100k',
         monthlyCost: 16,
         leftPercentage: 50
     },
     {
-        pageViews:'500K',
+        pageViews: '500k',
         monthlyCost: 24,
         leftPercentage: 75
     },
     {
-        pageViews:'1M',
+        pageViews: '1M',
         monthlyCost: 36,
         leftPercentage: 100
     },
@@ -42,44 +43,40 @@ form.addEventListener('submit', (e) => e.preventDefault())
 
 const getData = () => {
     const currentValue = rangeInput.value
-    const index = currentValue -1
-    return {pageViews, monthlyCost, leftPercentage} = VIEWS_DATA[index]
+    const index = currentValue - 1
+    return { pageViews, monthlyCost, leftPercentage } = VIEWS_DATA[index]
 };
-
 
 const updatePageViews = () => {
-    getData()
-    const {pageviews} = getData()
-    viewsContainer.textContent = '${pageViews} pageviews'
+    const { pageViews } = getData()
+    viewsContainer.textContent = `${pageViews} pageviews`
 };
 
-const isAnnualFrequency = () => { return frequencyToggle.checked};
+const isAnnualFrequency = () => { return frequencyToggle.checked };
 
 const updatePriceFrequency = (isAnnual) => {
-    if (isAnnual) {priceFreqContainer.textContent = '/year'}
-    else { priceFreqContainer.textContent = '/month'}
+    if (isAnnual) { priceFreqContainer.textContent = '/ year' }
+    else { priceFreqContainer.textContent = '/ month' }
 }
 
-const UpdateCost = () => {
-    const { monthlyCost} = getData()
+const updateCost = () => {
+    const { monthlyCost } = getData()
     const isAnnual = isAnnualFrequency()
     const price = isAnnual ? ((monthlyCost * 12) * 0.75) : monthlyCost
-    costContainer.textContent = '$${price.toFixed(2)}'
-    updatePriceFrequency(isAnnual) 
+    costContainer.textContent = `$${price.toFixed(2)}`
+    updatePriceFrequency(isAnnual)
 }
 
 const updateLeftPercentage = () => {
-    const {leftPercentage} = getData ()
+    const { leftPercentage } = getData()
     form.style.setProperty('--left', leftPercentage);
-}
+};
 
 const updateFormOnRangeInput = () => {
-    //Update page views
     updatePageViews()
-    //Update cost
-    UpdateCost()
-    //Update left variable
-}
+    updateCost()
+    updateLeftPercentage()
+};
 
-rangeInput.addEventListener('input, updateFormOnRangeInput')
-frequencyToggle.addEventListener('input', UpdateCost)
+rangeInput.addEventListener('input', updateFormOnRangeInput)
+frequencyToggle.addEventListener('input', updateCost)
